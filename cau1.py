9c12dc17c81018c10498046f410259bcca1b6cca
Cuong Liam <cuongdeptrai2706@gmail.com>
2025-12-11 21:39:24 +0700

    ep2

--- cau1.py
+++ cau1.py
@@ -12,9 +12,16 @@ for i in range(m):
         print(f"{matrix[i][j]}", end=" ")
     print()
 
+# for i in range(m):
+#     curr_sum = 0
+#     for j in range(n):
+#         curr_sum += matrix[i][j]
+#     if(curr_sum % 2 != 0):
+#         print(f"Dòng {i} (tổng : {curr_sum})")
+# cach 2:
+
 for i in range(m):
-    curr_sum = 0
-    for j in range(n):
-        curr_sum += matrix[i][j]
-    if(curr_sum % 2 != 0):
-        print(f"Dòng {i} (tổng : {curr_sum})")
+    row_sum = sum(matrix[i])
+
+    if(row_sum % 2 != 0):
+        print(f"Dòng {i} (tổng : {row_sum})")
